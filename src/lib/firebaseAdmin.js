fc3b784a188e42b20d5e41d7b998a272681ee755
yizhe <liaoyizhe75@gmail.com>
2025-07-24 10:04:04 +0800

    ✨ refactor: simplify Firebase admin initialization logic

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -2,15 +2,18 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const adminConfig = {
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-};
+// 防止被重複初始化
+const app = !getApps().length
+  ? initializeApp({
+      credential: cert({
+        // 這三組用你的環境變數取代
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    })
+  : getApp();
 
-const app = !getApps().length ? initializeApp(adminConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
