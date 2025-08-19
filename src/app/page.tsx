afcbe1851e4cf3e74045022dc5705f0a4f725b7a
yizhe <liaoyizhe75@gmail.com>
2025-08-19 11:52:12 +0800

    ✨ refactor: update profile retrieval logic in Home component to use useEffect and state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
+"use client"
 import StepperClient from "@/components/StepperClient";
 import liff from "@line/liff";
+import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function Home() {
-  let userId;
-  liff
-    .getProfile()
-    .then((profile) => {
-      userId = profile.userId;
-      console.log("User's ID:", userId);
-    })
-    .catch((err) => {
-      console.log("error", err);
-    });
-  toast.success("User's ID: " + userId);
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    if (liff.isLoggedIn()) {
+      liff.getProfile()
+        .then((profile) => {
+          setUserId(profile.userId);
+          toast.success("User's ID: " + profile.userId);
+          console.log("User's ID:", profile.userId);
+        })
+        .catch((err) => {
+          toast.error("取得 profile 失敗");
+          console.log("error", err);
+        });
+    } else {
+      toast.error("尚未登入 LINE");
+      console.log("尚未登入 LINE");
+    }
+  }, []);
+
   return (
     <div className="max-w-md mx-auto">
       <StepperClient />
+      <div>用戶 ID: {userId}</div>
     </div>
   );
 }
