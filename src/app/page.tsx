e4bc7b6ffdd214c6b5921b9a661f98d1de28a9c0
yizhe <liaoyizhe75@gmail.com>
2025-08-19 12:01:58 +0800

    ✨ refactor: integrate user ID management into step store and update profile retrieval in VerifyLIFF component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
 "use client"
 import StepperClient from "@/components/StepperClient";
-import liff from "@line/liff";
-import { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
+import { useStepStore } from "@/store/step-store";
 
 export default function Home() {
-  const [userId, setUserId] = useState("");
-
-  useEffect(() => {
-    if (liff.isLoggedIn()) {
-      liff.getProfile()
-        .then((profile) => {
-          setUserId(profile.userId);
-          toast.success("User's ID: " + profile.userId);
-          console.log("User's ID:", profile.userId);
-        })
-        .catch((err) => {
-          toast.error("取得 profile 失敗");
-          console.log("error", err);
-        });
-    } else {
-      toast.error("尚未登入 LINE");
-      console.log("尚未登入 LINE");
-    }
-  }, []);
+  const { userId } = useStepStore();
 
   return (
     <div className="max-w-md mx-auto">
